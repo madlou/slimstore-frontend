01dc39f7233b8b630af2b5a6fdc98d9997cd1568
Lewis Matthews <madlou@gmail.com>
2024-12-18 16:42:32 +0000

    updated demo pages

--- src/components/DemoInstructions.jsx
+++ src/components/DemoInstructions.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mantine/core'
+import { Box, Table, Text } from '@mantine/core'
 import React from 'react'
 
 const DemoInstructions = (props) => {
@@ -6,36 +6,78 @@ const DemoInstructions = (props) => {
         <Box>
             {
                 props.name == 'LOGIN' ? (
-                    <div>
-                        <p>{props.uiTranslations.devmessage1}</p>
-                        <p>{props.uiTranslations.devmessage2}</p>
-                        <table className='table-with-borders'><tbody>
-                            <tr><td>{props.uiTranslations.user}</td><td>{props.uiTranslations.password}</td><td>{props.uiTranslations.role}</td><td>{props.uiTranslations.store}</td></tr>
-                            <tr><td>1111</td><td>1234</td><td>{props.uiTranslations.associate}</td><td>423</td></tr>
-                            <tr><td>2222</td><td>1234</td><td>{props.uiTranslations.manager}*</td><td>423</td></tr>
-                            <tr><td>3333</td><td>1234</td><td>{props.uiTranslations.administrator}**</td><td>600</td></tr>
-                        </tbody></table>
-                        <p><i>* {props.uiTranslations.devmessage3}</i></p>
-                        <p><i>** {props.uiTranslations.devmessage4}</i></p>
-                    </div>
+                    <Box>
+                        <Text>{props.uiTranslations.devmessage1}</Text>
+                        <Text>{props.uiTranslations.devmessage2}</Text>
+                        <Table withTableBorder withColumnBorders>
+                            <Table.Thead>
+                                <Table.Tr>
+                                    <Table.Th>{props.uiTranslations.user}</Table.Th>
+                                    <Table.Th>{props.uiTranslations.password}</Table.Th>
+                                    <Table.Th>{props.uiTranslations.role}</Table.Th>
+                                    <Table.Th>{props.uiTranslations.store}</Table.Th>
+                                </Table.Tr>
+                            </Table.Thead>
+                            <Table.Tbody>
+                                <Table.Tr>
+                                    <Table.Td>1111</Table.Td>
+                                    <Table.Td>1234</Table.Td>
+                                    <Table.Td>{props.uiTranslations.associate}</Table.Td>
+                                    <Table.Td>423</Table.Td>
+                                </Table.Tr>
+                                <Table.Tr>
+                                    <Table.Td>2222</Table.Td>
+                                    <Table.Td>1234</Table.Td>
+                                    <Table.Td>{props.uiTranslations.manager}*</Table.Td>
+                                    <Table.Td>423</Table.Td></Table.Tr>
+                                <Table.Tr>
+                                    <Table.Td>3333</Table.Td>
+                                    <Table.Td>1234</Table.Td>
+                                    <Table.Td>{props.uiTranslations.administrator}**</Table.Td>
+                                    <Table.Td>600</Table.Td>
+                                </Table.Tr>
+                            </Table.Tbody>
+                        </Table>
+                        <Text fs="italic">* {props.uiTranslations.devmessage3}</Text>
+                        <Text fs="italic">** {props.uiTranslations.devmessage4}</Text>
+                    </Box>
                 ) : ''
             }
             {
                 props.name == 'REGISTER_CHANGE' ? (
-                    <div>
-                        <p>{props.uiTranslations.devmessage1}</p>
-                        <p>{props.uiTranslations.devmessage2}</p>
-                        <table className='table-with-borders'><tbody>
-                            <tr><td>{props.uiTranslations.store}</td><td>{props.uiTranslations.register}</td></tr>
-                            <tr><td>423</td><td>1</td></tr>
-                            <tr><td>423</td><td>2</td></tr>
-                            <tr><td>423</td><td>3</td></tr>
-                            <tr><td>600</td><td>1</td></tr>
-                        </tbody></table>
-                    </div>
+                    <Box>
+                        <Text>{props.uiTranslations.devmessage1}</Text>
+                        <Text>{props.uiTranslations.devmessage2}</Text>
+                        <Table withTableBorder withColumnBorders>
+                            <Table.Thead>
+                                <Table.Tr>
+                                    <Table.Td>{props.uiTranslations.store}</Table.Td>
+                                    <Table.Td>{props.uiTranslations.register}</Table.Td>
+                                </Table.Tr>
+                            </Table.Thead>
+                            <Table.Tbody>
+                                <Table.Tr>
+                                    <Table.Td>423</Table.Td>
+                                    <Table.Td>1</Table.Td>
+                                </Table.Tr>
+                                <Table.Tr>
+                                    <Table.Td>423</Table.Td>
+                                    <Table.Td>2</Table.Td>
+                                </Table.Tr>
+                                <Table.Tr>
+                                    <Table.Td>423</Table.Td>
+                                    <Table.Td>3</Table.Td>
+                                </Table.Tr>
+                                <Table.Tr>
+                                    <Table.Td>600</Table.Td>
+                                    <Table.Td>1</Table.Td>
+                                </Table.Tr>
+                            </Table.Tbody>
+                        </Table>
+                    </Box>
                 ) : ''
             }
-        </Box>
+        </Box >
     )
 }
 
