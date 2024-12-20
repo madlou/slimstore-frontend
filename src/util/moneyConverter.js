d343fcdc4159e02c32dd87913a9a10d607b71388
Lewis Matthews <madlou@gmail.com>
2024-12-20 09:43:30 +0000

    fix for foreign money conversions

--- src/util/moneyConverter.js
+++ src/util/moneyConverter.js
@@ -5,22 +5,23 @@ const moneyConverter = (countryCode, currency, value) => {
         PLN: 'zł',
         USD: '$',
     }
-    const sign = value < 0 ? '-' : '';
+    const sign = (value * 1) < 0 ? '-' : '';
+    value = Math.abs(value);
     switch (countryCode) {
         case 'DE':
-            return value.toFixed(2) + ' ' + currencyMap[currency];
+            return sign + value.toFixed(2) + ' ' + currencyMap[currency];
         case 'ES':
-            return value.toFixed(2) + ' ' + currencyMap[currency];
+            return sign + value.toFixed(2) + ' ' + currencyMap[currency];
         case 'FR':
-            return value.toFixed(2) + ' ' + currencyMap[currency];
+            return sign + value.toFixed(2) + ' ' + currencyMap[currency];
         case 'IE':
-            return sign + currencyMap[currency] + Math.abs(value).toFixed(2);
+            return sign + currencyMap[currency] + value.toFixed(2);
         case 'PL':
-            return value.toFixed(2) + ' ' + currencyMap[currency];
+            return sign + value.toFixed(2) + ' ' + currencyMap[currency];
         case 'UK':
-            return sign + currencyMap[currency] + Math.abs(value).toFixed(2);
+            return sign + currencyMap[currency] + value.toFixed(2);
         case 'US':
-            return value.toFixed(2) + ' ' + currencyMap[currency];
+            return sign + value.toFixed(2) + ' ' + currencyMap[currency];
     }
 }
 
