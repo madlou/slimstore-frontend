3677054383a917bc246cce78b1ca866f1ad4f85d
Lewis Matthews <madlou@gmail.com>
2025-01-06 15:10:05 +0000

    main root change

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -55,106 +55,136 @@ function Main() {
         api.post(setResponse, requestForm, lang);
     }, [requestForm]);
     return (
-        <>
-            <AppShell
-                header={{ height: 16 * 3 }}
-                footer={{ height: footerHeight }}
-                padding='md'
+        <AppShell
+            header={{ height: 16 * 3 }}
+            footer={{ height: footerHeight }}
+            padding='md'
+        >
+            <AppShell.Header
+                bg='rgba(0, 0, 0, .04)'
             >
-                <AppShell.Header
-                    bg='rgba(0, 0, 0, .04)'
+                <Group
+                    pl='md'
+                    pr='md'
+                    justify='space-between'
+                    style={{ flexWrap: 'nowrap' }}
                 >
                     <Group
-                        pl='md'
-                        pr='md'
-                        justify='space-between'
                         style={{ flexWrap: 'nowrap' }}
                     >
-                        <Group
-                            style={{ flexWrap: 'nowrap' }}
-                        >
-                            <Title
-                                order={1}
-                                pt={5}
-                                textWrap='nowrap'
-                            >{response.uiTranslations.logo}</Title>
-                            <Title
-                                order={2}
-                                pt={10}
-                                fz={22}
-                                textWrap='nowrap'
-                                visibleFrom='lg'
-                            >{response.uiTranslations.header} - </Title>
-                            <Title
-                                order={2}
-                                pt={10}
-                                fz={22}
-                                textWrap='nowrap'
-                            >{response.view.title}</Title>
-                        </Group>
-                        <Box visibleFrom='sm'>
-                            <MenuButtons
-                                footerHeight={footerHeight}
-                                setFooterHeight={setFooterHeight}
-                                menuOpened={menuOpened}
-                                layout={layout}
-                                setLayout={setLayout}
-                                lang={lang}
-                                setLang={setLang}
-                                response={response}
-                                requestForm={requestForm}
-                                setRequestForm={setRequestForm}
-                                showKeyboard={showKeyboard}
-                                setShowKeyboard={setShowKeyboard}
-                            />
-                        </Box>
-                        <Drawer
-                            opened={menuOpened}
-                            onClose={() => { setMenuOpened(false) }}
-                            size='40%'
-                            padding='md'
-                            position='right'
-                            title={response.uiTranslations.header}
-                            hiddenFrom='sm'
-                            zIndex={100}
-                        >
-                            <MenuButtons
-                                footerHeight={footerHeight}
-                                setFooterHeight={setFooterHeight}
-                                menuOpened={menuOpened}
-                                layout={layout}
-                                setLayout={setLayout}
-                                lang={lang}
-                                setLang={setLang}
-                                response={response}
-                                requestForm={requestForm}
-                                setRequestForm={setRequestForm}
-                                showKeyboard={showKeyboard}
-                                setShowKeyboard={setShowKeyboard}
-                            />
-                        </Drawer>
-                        <Burger
-                            opened={menuOpened}
-                            onClick={() => { setMenuOpened(!menuOpened) }}
-                            hiddenFrom='sm'
-                            size='md'
-                        />
+                        <Title
+                            order={1}
+                            pt={5}
+                            textWrap='nowrap'
+                        >{response.uiTranslations.logo}</Title>
+                        <Title
+                            order={2}
+                            pt={10}
+                            fz={22}
+                            textWrap='nowrap'
+                            visibleFrom='lg'
+                        >{response.uiTranslations.header} - </Title>
+                        <Title
+                            order={2}
+                            pt={10}
+                            fz={22}
+                            textWrap='nowrap'
+                        >{response.view.title}</Title>
                     </Group>
-                </AppShell.Header>
-                <AppShell.Main
-                    bg='rgba(150, 150, 150, .2)'
-                    display='flex'
-                >
-                    {layout[0] == 12 ? (
-                        <Stack
-                            w={'100%'}
+                    <Box visibleFrom='sm'>
+                        <MenuButtons
+                            footerHeight={footerHeight}
+                            setFooterHeight={setFooterHeight}
+                            menuOpened={menuOpened}
+                            layout={layout}
+                            setLayout={setLayout}
+                            lang={lang}
+                            setLang={setLang}
+                            response={response}
+                            requestForm={requestForm}
+                            setRequestForm={setRequestForm}
+                            showKeyboard={showKeyboard}
+                            setShowKeyboard={setShowKeyboard}
+                        />
+                    </Box>
+                    <Drawer
+                        opened={menuOpened}
+                        onClose={() => { setMenuOpened(false) }}
+                        size='40%'
+                        padding='md'
+                        position='right'
+                        title={response.uiTranslations.header}
+                        hiddenFrom='sm'
+                        zIndex={100}
+                    >
+                        <MenuButtons
+                            footerHeight={footerHeight}
+                            setFooterHeight={setFooterHeight}
+                            menuOpened={menuOpened}
+                            layout={layout}
+                            setLayout={setLayout}
+                            lang={lang}
+                            setLang={setLang}
+                            response={response}
+                            requestForm={requestForm}
+                            setRequestForm={setRequestForm}
+                            showKeyboard={showKeyboard}
+                            setShowKeyboard={setShowKeyboard}
+                        />
+                    </Drawer>
+                    <Burger
+                        opened={menuOpened}
+                        onClick={() => { setMenuOpened(!menuOpened) }}
+                        hiddenFrom='sm'
+                        size='md'
+                    />
+                </Group>
+            </AppShell.Header>
+            <AppShell.Main
+                bg='rgba(150, 150, 150, .2)'
+                display='flex'
+            >
+                {layout[0] == 12 ? (
+                    <Stack
+                        w={'100%'}
+                    >
+                        <Display
+                            layout={layout}
+                            response={response}
+                        />
+                        <Form
+                            api={api}
+                            response={response}
+                            formElements={formElements}
+                            inputFocused={inputFocused}
+                            layout={layout}
+                            setInputFocused={setInputFocused}
+                            setRequestForm={setRequestForm}
+                            showKeyboard={showKeyboard}
+                            updateFormElements={updateFormElements}
+                        />
+                    </Stack>
+                ) : (
+                    <Grid
+                        flex={1}
+                        display={layout[0] == 12 ? 'flex' : 'flex'}
+                    >
+                        <Grid.Col
+                            span={layout[0]}
+                            display={layout[0] == 12 ? 'block' : 'flex'}
+                            flex={layout[0] == 12 ? 'unset' : 1}
                         >
                             <Display
                                 layout={layout}
                                 response={response}
                             />
+                        </Grid.Col>
+                        <Grid.Col
+                            span={layout[1]}
+                            display={layout[1] == 12 ? 'block' : 'flex'}
+                            flex={layout[0] == 12 ? 'unset' : 1}
+                        >
                             <Form
-                                api={api}
                                 response={response}
                                 formElements={formElements}
                                 inputFocused={inputFocused}
@@ -164,64 +194,32 @@ function Main() {
                                 showKeyboard={showKeyboard}
                                 updateFormElements={updateFormElements}
                             />
-                        </Stack>
-                    ) : (
-                        <Grid
-                            flex={1}
-                            display={layout[0] == 12 ? 'flex' : 'flex'}
-                        >
-                            <Grid.Col
-                                span={layout[0]}
-                                display={layout[0] == 12 ? 'block' : 'flex'}
-                                flex={layout[0] == 12 ? 'unset' : 1}
-                            >
-                                <Display
-                                    layout={layout}
-                                    response={response}
-                                />
-                            </Grid.Col>
-                            <Grid.Col
-                                span={layout[1]}
-                                display={layout[1] == 12 ? 'block' : 'flex'}
-                                flex={layout[0] == 12 ? 'unset' : 1}
-                            >
-                                <Form
-                                    response={response}
-                                    formElements={formElements}
-                                    inputFocused={inputFocused}
-                                    layout={layout}
-                                    setInputFocused={setInputFocused}
-                                    setRequestForm={setRequestForm}
-                                    showKeyboard={showKeyboard}
-                                    updateFormElements={updateFormElements}
-                                />
-                            </Grid.Col>
-                        </Grid>
-                    )}
-                </AppShell.Main>
-                <AppShell.Footer
-                    bg='rgba(0, 0, 0, .05)'
-                >
-                    <Keyboard
-                        inputFocused={inputFocused}
-                        updateFormElements={updateFormElements}
-                        viewName={viewName}
-                        showKeyboard={showKeyboard}
-                    />
-                    <FunctionButtons
-                        formElements={formElements}
-                        response={response}
-                        setRequestForm={setRequestForm}
-                    />
-                    <StatusBar
-                        store={response.store}
-                        register={response.register}
-                        uiTranslations={response.uiTranslations}
-                        user={response.user}
-                    />
-                </AppShell.Footer>
-            </AppShell >
-        </>
+                        </Grid.Col>
+                    </Grid>
+                )}
+            </AppShell.Main>
+            <AppShell.Footer
+                bg='rgba(0, 0, 0, .05)'
+            >
+                <Keyboard
+                    inputFocused={inputFocused}
+                    updateFormElements={updateFormElements}
+                    viewName={viewName}
+                    showKeyboard={showKeyboard}
+                />
+                <FunctionButtons
+                    formElements={formElements}
+                    response={response}
+                    setRequestForm={setRequestForm}
+                />
+                <StatusBar
+                    store={response.store}
+                    register={response.register}
+                    uiTranslations={response.uiTranslations}
+                    user={response.user}
+                />
+            </AppShell.Footer>
+        </AppShell >
     )
 }
 
